655f58daf023e22c58a7679e0df0ea9239a49d7b
yol <yoltorres2@gmail.com>
2020-10-05 22:33:38 +0800

    upload done

--- src/templates/base/forms/Upload.js
+++ src/templates/base/forms/Upload.js
@@ -14,22 +14,21 @@ const BasicForms = () => {
   const [file, setFile] = useState([]);
   const [description, setDescription] = useState("");
   const handleFileChange = (e) => {
-    let { value } = e.target;
-    console.log(value);
-    setFile(value);
+    let { files } = e.target;
+    console.log(files)
+    setFile(files[0]);
   };
   const handleDescriptionChange = (e) => {
     let { value } = e.target;
     setDescription(value);
   };
 
+
   const handleSubmit = async () => {
     let formdata = new FormData();
-    formdata.append("file", file);
-    formdata.append("description", description);
-    console.log(formdata);
-    let res = await api.post("/upload", {});
-    console.log(res);
+    formdata.append("file", file , file.name);
+    formdata.append("description" , description)
+    let res = await api.post("/upload",formdata , true);
   };
   return (
     <>
@@ -41,7 +40,6 @@ const BasicForms = () => {
 
           <CCol xs="12" md="9">
             <CInputFile
-              value={file}
               id="file-input"
               onChange={handleFileChange}
               name="file"
