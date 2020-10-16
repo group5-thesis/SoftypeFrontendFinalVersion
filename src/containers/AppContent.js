70f7e6b831bd5b1c62fa2c867866631a099e31e5
yol <yoltorres2@gmail.com>
2020-10-16 22:42:01 +0800

    to merge

--- src/containers/AppContent.js
+++ src/containers/AppContent.js
@@ -27,15 +27,14 @@ const AppContent = (_props) => {
     dispatch(actionCreator(ActionTypes.FETCH_LEAVE_REQUEST));
     let res = await api.post("/getLeaveRequest", payload);
     if (!res.error) {
-      // let {leave_requests} = res.data;
-      console.log("ok na ")
+      let {leave_requests} = res.data;
     }
   }
 
   const fetchTickets = async () => {
     let response = await api.get('/retrieve_tickets')
     if (response.error) {
-      console.log('error');
+      console.log(response.message);
     }
     else {
       var temp = response.data.ticket_information;
