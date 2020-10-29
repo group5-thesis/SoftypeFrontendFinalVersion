5aed05b205b1636b48b04744566a020be0ea2a82
Student Admin <student.admin@pn.local>
2020-10-29 19:54:06 +0800

    commit

--- src/modules/calendar/Calendar.js
+++ src/modules/calendar/Calendar.js
@@ -1,34 +1,67 @@
 import React from 'react'
-import { Calendar, Views } from 'react-big-calendar'
-// import events from '../events'
-import * as dates from './dates'
+// import { momentLocalizer, Views } from 'react-big-calendar'
+import events from './events'
+// import * as dates from './dates'
+// import moment from 'moment'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import views from 'views';
+import { CCard, CCardBody } from '@coreui/react';
 
-let allViews = Object.keys(Views).map(k => Views[k])
+const localizer = momentLocalizer(moment)
 
-const ColoredDateCellWrapper = ({ children }) =>
-  React.cloneElement(React.Children.only(children), {
-    style: {
-      backgroundColor: 'lightblue',
-    },
-  })
 
-let Basic = ({localizer}) => (
-  <Calendar
-    // events={events}+
+// let allViews = Object.keys(Views).map(k => Views[k])
 
-    views={allViews}
-    step={60}
-    showMultiDayTimes
-    max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
-    defaultDate={new Date(2015, 3, 1)}
-    components={{
-      timeSlotWrapper: ColoredDateCellWrapper,
-    }}
-    localizer={localizer}
-    
-  />
-  
+// const ColoredDateCellWrapper = ({ children }) =>
+//   React.cloneElement(React.Children.only(children), {
+//     style: {
+//       backgroundColor: 'lightblue'
+//     },
+//   })
+
+const Basic = props => (
+  // const localizer =  Calendar.momentLocalizer(moment)
+  // return(
+  <div>
+    <CCard>
+      <CCardBody>
+        <Calendar
+          localizer={localizer}
+          // view= {views.month}
+          events={events}
+
+          startAccessor='start'
+          endAccessor='end'
+          style={{ height: 600, backgroundColor: 'white' }}
+
+        // events={events}
+        // culture = 'en-GB'
+        // views={allViews}
+        // step={60}
+        // showMultiDayTimes
+        // max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
+        // defaultDate={new Date(2015, 3, 1)}
+        // components={{
+        //   timeSlotWrapper: ColoredDateCellWrapper,
+        // }}
+        // localizer={localizer}
+
+
+        />
+      </CCardBody>
+    </CCard>
+
+    {/* <CCard
+       events = {events}
+    /> */}
+  </div>
 )
+// }
+
+
+
 
 
 export default Basic
