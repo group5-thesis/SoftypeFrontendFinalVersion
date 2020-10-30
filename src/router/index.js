5d000fe277d1de7b26a3e076f98709c6a87a6032
PN\student.admin <rangie.laurente@mytikasllc.com>
2020-10-30 22:27:20 +0800

    added

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,7 @@ const Repository = React.lazy(() => import('modules/repository/Repositoryv1'))
 const RepositoryFiles = React.lazy(() => import('modules/repository/RepositoryFiles'))
 const Upload = React.lazy(() => import('templates/base/forms/Upload'))
 const Cards = React.lazy(() => import('templates/base/cards/Cards'))
+<<<<<<< HEAD
 const Calendar = React.lazy(() => import('modules/calendar/Calendar'))
 const Ticket = React.lazy(() => import('modules/ticket/Ticket'))
 const PerformanceReview = React.lazy(() => import('modules/performance-review/PerformanceReview'))
@@ -28,6 +29,11 @@ const Department = React.lazy(() => import('modules/departments/component/Depart
  * 3 - regular employee
  * 4 - all
  */
+=======
+const Calendar = React.lazy(() => import('views/calendar/Calendar'))
+const Test =  React.lazy(() => import('views/test/fileupload'));
+//path = url, 
+>>>>>>> gogoo
 const routes = [
     { path: '/', exact: true, name: 'Home', user: [4] },
     { path: '/dashboard', name: 'Dashboard', component: Dashboard, user: [4] },
@@ -44,9 +50,13 @@ const routes = [
     // { path: '/chart', name: 'Organizational Chart', component: Test, user: [4] },
     { path: '/cards', name: 'Cards', component: Cards, user: [4] },
     { path: '/calendar', name: 'Calendar', component: Calendar, user: [4] },
+<<<<<<< HEAD
     { path: '/upload', name: 'Upload', component: Upload, user: [4] },
     { path: '/ticket', exact: true, name: 'Ticket', component: Ticket, user: [4] },
     { path: '/employee/departments', exact: true, name: 'Departments', component: Departments, user: [4] }
+=======
+    { path: '/upload', name: 'FIle Upload', component: Test, user: [4] },
+>>>>>>> gogoo
 
 ]
 
