e3f8f4b7e854b1bb34d34f44ff259fda7e406b71
Student Admin <student.admin@pn.local>
2020-11-02 11:04:23 +0800

    commit

--- src/modules/leave-management/LeaveCalendar.js
+++ src/modules/leave-management/LeaveCalendar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Calendar from 'modules/calendar/Calendar'
+// import {header} from './../calendar/Calendar'
 class LeaveCalendar extends Component {
   render() {
     return <Calendar {...{
--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL_DEV: "https://e20855a69e94.ngrok.io/api",
+  API_URL_DEV: "https://eb907a0efaa5.ngrok.io/api",
   API_URL_LIVE: ""
 };
   
