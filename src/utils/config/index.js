bb28c60b765447f20f2cc60d9d2c7ac303cf34c0
Student Admin <student.admin@pn.local>
2020-11-03 15:47:33 +0800

    commit

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,6 +1,6 @@
 export const config = {
-    API_URL_DEV:  "https://a389375714b9.ngrok.io/api",
+    API_URL_DEV:  "https://227620ae240c.ngrok.io/api",
     API_URL_LIVE: "",
-    // API_URL_BASE_LIVE : "",
-    // API_URL_BASE_DEV : "http://localhost:8000",
+    API_URL_BASE_LIVE : "",
+    API_URL_BASE_DEV : "http://localhost:8000",
 };
