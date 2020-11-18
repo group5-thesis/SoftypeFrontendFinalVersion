b8d9a89f69bf00d8bd4c400c98509b41a0bf095c
PN\student.admin <rangie.laurente@mytikasllc.com>
2020-11-18 23:20:00 +0800

    Dashboard -G

--- src/modules/dashboard/Dashboard.js
+++ src/modules/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react'
+import React, { lazy, useState, useEffect } from 'react'
 import {
   CBadge,
   CButton,
@@ -8,138 +8,159 @@ import {
   CCol,
   CProgress,
   CRow,
-  CCallout
+  CCallout,
+  CDataTable
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
+import { useSelector, useDispatch } from 'react-redux';
+import { copyArray, setWidth, toCapitalize, shallowCopy, RULES, getBaseUrl } from 'utils/helpers';
+import { NoData, Card, Modal } from 'reusable';
+import { useHistory } from "react-router-dom";
 
 const Widgets = lazy(() => import('../dashboard/component/Widget.js'))
-const PieGraph = lazy(() => import('../dashboard/component/PieGraph.js'))
 
 const Dashboard = () => {
+
+  const usersData = [
+    // { id: 0, name: 'John Doe', registered: '2018/01/01', role: 'Guest', status: 'Pending' },
+    // { id: 1, name: 'Samppa Nori', registered: '2018/01/01', role: 'Member', status: 'Active' },
+    // { id: 2, name: 'Test Nori', registered: '2018/01/01', role: 'Member', status: 'Active' },
+    // { id: 3, name: 'TEst Nori', registered: '2018/01/01', role: 'Member', status: 'Active' },
+    // { id: 4, name: 'TEstse Nori', registered: '2018/01/01', role: 'Member', status: 'Active' },
+  ]
+
+  const fields = [
+    { key: 'name', _style: { width: '40%' } },
+    { key: 'date', _style: { width: '20%' } },
+    { key: 'status', _style: { width: '20%' } },
+  ]
+
+  const getBadge = (status) => {
+    switch (status) {
+      case 'Active': return 'success'
+      case 'Inactive': return 'secondary'
+      case 'Pending': return 'warning'
+      case 'Banned': return 'danger'
+      default: return 'primary'
+    }
+  }
+
+  const stateActiveEmployees = useSelector((state) => {
+    return state.appState.employee.employees.filter(emp => {
+      return emp.isActive === 1
+    })
+  });
+
+  const stateLeaveRequests = useSelector((state) => {
+    return state.appState.leave.leave_requests
+  });
+
+  const history = useHistory();
+  const [totalEmployees, setTotalEmployees] = useState(stateActiveEmployees.length)
+  const [totalLeave, setTotalLeave] = useState(stateLeaveRequests)
+
+  const viewEmployees = () => {
+    history.push(`/employees`);
+  }
+
+  const viewLeaveCalendar = () => {
+    history.push(`/leave/calendar`);
+  }
+
+  const viewLeaveRequests = () => {
+    history.push(`/leave/requests`);
+  }
+
+  const viewOfficeRequests = () => {
+    history.push(`/requests`);
+  }
+
+  useEffect(() => {
+    return
+  }, [totalEmployees, totalLeave])
+
   return (
     <>
-      <Widgets />
+      <Widgets {...{ totalEmployees, viewLeaveRequests, viewOfficeRequests, viewEmployees, viewLeaveCalendar }} />
       <CRow>
         <CCol>
           <CCard>
             <CCardHeader>
-              Traffic {' & '} Sales
-            </CCardHeader>
-            <CCardBody>
               <CRow>
-                <CCol xs="12" md="6" xl="6">
-                  <CRow>
-                    <CCol sm="6">
-                      <CCallout color="info">
-                        <small className="text-muted">New Clients</small>
-                        <br />
-                        <strong className="h4">9,123</strong>
-                      </CCallout>
-                    </CCol>
-                    <CCol sm="6">
-                      <CCallout color="danger">
-                        <small className="text-muted">Recurring Clients</small>
-                        <br />
-                        <strong className="h4">22,643</strong>
-                      </CCallout>
-                    </CCol>
-                  </CRow>
-
-                  <hr className="mt-0" />
-
-                  <div className="progress-group mb-4">
-                    <div className="progress-group-prepend">
-                      <span className="progress-group-text">
-                        Monday
-                      </span>
-                    </div>
-                    <div className="progress-group-bars">
-                      <CProgress className="progress-xs" color="info" value="34" />
-                      <CProgress className="progress-xs" color="danger" value="78" />
-                    </div>
-                  </div>
-                  <div className="progress-group mb-4">
-                    <div className="progress-group-prepend">
-                      <span className="progress-group-text">
-                        Tuesday
-                      </span>
-                    </div>
-                    <div className="progress-group-bars">
-                      <CProgress className="progress-xs" color="info" value="56" />
-                      <CProgress className="progress-xs" color="danger" value="94" />
-                    </div>
-                  </div>
-                  <div className="progress-group mb-4">
-                    <div className="progress-group-prepend">
-                      <span className="progress-group-text">
-                        Wednesday
-                      </span>
-                    </div>
-                    <div className="progress-group-bars">
-                      <CProgress className="progress-xs" color="info" value="12" />
-                      <CProgress className="progress-xs" color="danger" value="67" />
-                    </div>
-                  </div>
-                  <div className="progress-group mb-4">
-                    <div className="progress-group-prepend">
-                      <span className="progress-group-text">
-                        Thursday
-                      </span>
-                    </div>
-                    <div className="progress-group-bars">
-                      <CProgress className="progress-xs" color="info" value="43" />
-                      <CProgress className="progress-xs" color="danger" value="91" />
-                    </div>
-                  </div>
-                  <div className="progress-group mb-4">
-                    <div className="progress-group-prepend">
-                      <span className="progress-group-text">
-                        Friday
-                      </span>
-                    </div>
-                    <div className="progress-group-bars">
-                      <CProgress className="progress-xs" color="info" value="22" />
-                      <CProgress className="progress-xs" color="danger" value="73" />
-                    </div>
-                  </div>
-                  <div className="progress-group mb-4">
-                    <div className="progress-group-prepend">
-                      <span className="progress-group-text">
-                        OL
-                      </span>
-                    </div>
-                    <div className="progress-group-bars">
-                      <CProgress className="progress-xs" color="info" value="53" />
-                      <CProgress className="progress-xs" color="danger" value="82" />
-                    </div>
-                  </div>
-                  <div className="progress-group mb-4">
-                    <div className="progress-group-prepend">
-                      <span className="progress-group-text">
-                        Sunday
-                      </span>
-                    </div>
-                    <div className="progress-group-bars">
-                      <CProgress className="progress-xs" color="info" value="9" />
-                      <CProgress className="progress-xs" color="danger" value="69" />
-                    </div>
-                  </div>
-                  <div className="legend text-center">
-                    <small>
-                      <sup className="px-1"><CBadge shape="pill" color="info">&nbsp;</CBadge></sup>
-                      New clients
-                      &nbsp;
-                      <sup className="px-1"><CBadge shape="pill" color="danger">&nbsp;</CBadge></sup>
-                      Recurring clients
-                    </small>
+                <CCol sm="6">
+                  <div>
+                    {"Recent Leave Requests"}
                   </div>
                 </CCol>
-
-                <CCol xs="12" md="6" xl="6">
-                    <PieGraph />
+                <CCol sm="6" className="d-none d-md-block">
+                  <div className="float-right">
+                    <CButton size="sm" color="primary" onClick={() => {
+                      viewLeaveRequests()
+                    }} disabled={false}>
+                      {"View All"}
+                    </CButton>
+                  </div>
                 </CCol>
               </CRow>
-              <br />
+            </CCardHeader>
+            <CCardBody>
+              <CDataTable
+                items={usersData}
+                fields={fields}
+                hover
+                noItemsViewSlot={<NoData title="No Requests"/>}
+                scopedSlots={{
+                  'status':
+                    (item) => (
+                      <td>
+                        <CBadge color={getBadge(item.status)}>
+                          {item.status}
+                        </CBadge>
+                      </td>
+                    ),
+                }}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              <CRow>
+                <CCol sm="6">
+                  <div>
+                    {"Recent Office Requests"}
+                  </div>
+                </CCol>
+                <CCol sm="6" className="d-none d-md-block">
+                  <div className="float-right">
+                    <CButton size="sm" color="primary" onClick={() => {
+                      viewOfficeRequests()
+                    }} disabled={false}>
+                      {"View All"}
+                    </CButton>
+                  </div>
+                </CCol>
+              </CRow>
+
+            </CCardHeader>
+            <CCardBody>
+              <CDataTable
+                items={usersData}
+                fields={fields}
+                hover
+                noItemsViewSlot={<NoData title="No Requests" />}
+                scopedSlots={{
+                  'status':
+                    (item) => (
+                      <td>
+                        <CBadge color={getBadge(item.status)}>
+                          {item.status}
+                        </CBadge>
+                      </td>
+                    ),
+                }}
+              />
             </CCardBody>
           </CCard>
         </CCol>
--- src/modules/dashboard/component/Widget.js
+++ src/modules/dashboard/component/Widget.js
@@ -11,11 +11,11 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
-const Widgets = () => {
+const Widgets = ({ totalEmployees, viewLeaveRequests, viewOfficeRequests, viewEmployees, viewLeaveCalendar }) => {
 
   return (
     <CRow>
-       <CCol sm="6" lg="3">
+      <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-primary"
           header="9.823"
@@ -23,17 +23,18 @@ const Widgets = () => {
           footerSlot={
             <div
               className={'text-center'}
-              style={{ height: '100px' }}
+              style={{ height: '50px' }}
             >
             </div>
           }
         >
           <CDropdown>
             <CDropdownToggle color="transparent">
-              <CIcon name="cil-settings" />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>View Leave Calendar</CDropdownItem>
+              <CDropdownItem onClick={() => {
+                viewLeaveCalendar()
+              }}>View Leave Calendar</CDropdownItem>
             </CDropdownMenu>
           </CDropdown>
         </CWidgetDropdown>
@@ -42,27 +43,27 @@ const Widgets = () => {
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-info"
-          header="9.823"
+          header={`${totalEmployees}`}
           text="Total Employees"
           footerSlot={
             <div
               className={'text-center'}
-              style={{ height: '100px' }}
+              style={{ height: '50px' }}
             >
             </div>
           }
         >
           <CDropdown>
             <CDropdownToggle color="transparent">
-              <CIcon name="cil-settings" />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>View Employees</CDropdownItem>
+              <CDropdownItem onClick={() => {
+                viewEmployees()
+              }}>View Employees</CDropdownItem>
             </CDropdownMenu>
           </CDropdown>
         </CWidgetDropdown>
       </CCol>
-
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-warning"
@@ -71,22 +72,22 @@ const Widgets = () => {
           footerSlot={
             <div
               className={'text-center'}
-              style={{ height: '100px' }}
+              style={{ height: '50px' }}
             >
             </div>
           }
         >
           <CDropdown>
             <CDropdownToggle color="transparent">
-              <CIcon name="cil-settings" />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>View Office Requests</CDropdownItem>
+              <CDropdownItem onClick={() => {
+                viewOfficeRequests()
+              }}>View Office Requests</CDropdownItem>
             </CDropdownMenu>
           </CDropdown>
         </CWidgetDropdown>
       </CCol>
-
       <CCol sm="6" lg="3">
         <CWidgetDropdown
           color="gradient-danger"
@@ -95,17 +96,18 @@ const Widgets = () => {
           footerSlot={
             <div
               className={'text-center'}
-              style={{ height: '100px' }}
+              style={{ height: '50px' }}
             >
             </div>
           }
         >
           <CDropdown>
             <CDropdownToggle caret className="text-white" color="transparent">
-              <CIcon name="cil-settings" />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>View Leave Requests</CDropdownItem>
+              <CDropdownItem onClick={() => {
+                viewLeaveRequests()
+              }}>View Leave Requests</CDropdownItem>
             </CDropdownMenu>
           </CDropdown>
         </CWidgetDropdown>
