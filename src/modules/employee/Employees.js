cf331a55b0137e6e137225a7094778a67c5fb579
Dev-Yol <leonilojr.torres@student.passerellesnumeriques.org>
2020-12-07 15:53:23 +0800

    update

--- src/modules/employee/Employees.js
+++ src/modules/employee/Employees.js
@@ -24,11 +24,11 @@ const getBadge = status => {
 let headers = [
     { key: 'Name', _classes: 'font-weight-bold', _style: { width: "20%" }, },
     {
-        key: 'mobileno', label: "Mobile No.", _style: { width: "10%" }, 
+        key: 'mobileno', label: "Mobile No.", _style: { width: "10%" },
         sorter: false,
         filter: false,
     },
-    { key: 'email' },
+    // { key: 'email', _style: { width: "10%" } },
     {
         key: "role",
         label: "Position",
