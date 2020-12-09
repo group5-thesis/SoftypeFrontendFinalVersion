b83cf5860e970a10ec5386e1fb896b403857b5d0
Dev-Yol <leonilojr.torres@student.passerellesnumeriques.org>
2020-12-09 20:51:54 +0800

    update

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
     API_URL_DEV: "http://localhost:8000/api",
-    API_URL_LIVE: "https://ec2-3-0-177-104.ap-southeast-1.compute.amazonaws.com:8000/api",
-    API_URL_BASE_LIVE: "https://ec2-3-0-177-104.ap-southeast-1.compute.amazonaws.com:8000",
+    API_URL_LIVE: "http://ec2-3-0-177-104.ap-southeast-1.compute.amazonaws.com:8000/api",
+    API_URL_BASE_LIVE: "http://ec2-3-0-177-104.ap-southeast-1.compute.amazonaws.com:8000",
     API_URL_BASE_DEV: "http://localhost:8000",
     IS_DEV:false,// process.env.NODE_ENV === 'development',
     PUSHER: {
